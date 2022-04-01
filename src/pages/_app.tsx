fe5fe8763e7dcbf654c1299b139fd02fd2fc2499
Patricia Bueno <patibuenodev@gmail.com>
2022-04-01 18:31:00 -0300

    adding header and hero

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
+import { Header } from "../components/Header";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Ig News</title>
-      </Head>
+    <Header/>
       <Component {...pageProps} />
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <h2 className={styles.title}>Hello World!!!</h2>;
+  return (
+    <>
+      <Head>
+        <title>Home Ig News</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
