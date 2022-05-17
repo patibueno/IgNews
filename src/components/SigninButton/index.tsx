1c54338a6e2cf711c38c43ab345fa1ac71c0d365
Patricia Bueno <patibuenodev@gmail.com>
2022-05-17 10:36:18 -0300

    faunadb config

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -5,7 +5,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SignInButton() {
   const {data: session} = useSession();
-  console.log(session);
+
   return session ? (
     <button
     type="button"
